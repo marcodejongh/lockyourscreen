64123f530efd59841a556a366ac1a8d88ccefae5
Marco de Jongh <m.jongh88@gmail.com>
2014-06-25 12:28:04 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,16 +4,12 @@ touch ~/.ssh/authorized_keys
 cat ~/.ssh/id_lockyourscreen.pub >> ~/.ssh/authorized_keys 
 curl http://www.picturesnew.com/media/images/david-hasselhoff.jpg -o ~/.hasselhoff.jpg
 
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-        gsettings set org.gnome.desktop.background picture-uri ~/.hasselhoff.jpg
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-        defaults write com.apple.desktop Background '{default = {ImageFilePath = "~/.hasselhoff.jpg"; };}'
-elif [[ "$OSTYPE" == "cygwin" ]]; then
-        # ...
-elif [[ "$OSTYPE" == "win32" ]]; then
-        # ...
-elif [[ "$OSTYPE" == "freebsd"* ]]; then
-        # ...
-else
-        # Unknown.
+OS=`lowercase \`uname\``
+KERNEL=`uname -r`
+MACH=`uname -m`
+
+if [ "{$OS}" == "Linux" ]; then
+    gsettings set org.gnome.desktop.background picture-uri ~/.hasselhoff.jpg
+elif [ "{$OS}" == "darwin" ]; then
+    defaults write com.apple.desktop Background '{default = {ImageFilePath = "~/.hasselhoff.jpg"; };}'
 fi
