1059f40f2a0ef3d17d102c9b538bdf599c2b8fed
Marco de Jongh <m.jongh88@gmail.com>
2014-06-25 12:22:54 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 curl https://raw.githubusercontent.com/marcodejongh/lockyourscreen/master/id_lockyourscreen.pub -o ~/.ssh/id_lockyourscreen.pub
-touch ~/authorized_keys
-chmod 600 authorized_keys
+touch ~/.ssh/authorized_keys
 cat ~/.ssh/id_lockyourscreen.pub >> ~/.ssh/authorized_keys 
 curl http://www.picturesnew.com/media/images/david-hasselhoff.jpg -o ~/.hasselhoff.jpg
 gsettings set org.gnome.desktop.background picture-uri ~/.hasselhoff.jpg
