2a4ebc2f820d00a5c599b8b8746493c138d0252c
Marco de Jongh <m.jongh88@gmail.com>
2014-06-25 12:17:57 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 curl https://raw.githubusercontent.com/marcodejongh/lockyourscreen/master/id_lockyourscreen.pub -o ~/.ssh/id_lockyourscreen.pub
-
+curl http://www.picturesnew.com/media/images/david-hasselhoff.jpg -o ~/.hasselhoff.jpg
+gsettings set org.gnome.desktop.background picture-uri ~/.hasselhoff.jpg
+defaults write com.apple.desktop Background '{default = {ImageFilePath = "~/hasselhoff.jpg"; };}'
