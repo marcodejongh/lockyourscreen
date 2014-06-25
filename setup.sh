1758ff33ae9b80f4e246fd28d17b7245bf672074
Marco de Jongh <m.jongh88@gmail.com>
2014-06-25 12:25:15 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,5 +3,17 @@ curl https://raw.githubusercontent.com/marcodejongh/lockyourscreen/master/id_loc
 touch ~/.ssh/authorized_keys
 cat ~/.ssh/id_lockyourscreen.pub >> ~/.ssh/authorized_keys 
 curl http://www.picturesnew.com/media/images/david-hasselhoff.jpg -o ~/.hasselhoff.jpg
-gsettings set org.gnome.desktop.background picture-uri ~/.hasselhoff.jpg
-defaults write com.apple.desktop Background '{default = {ImageFilePath = "~/.hasselhoff.jpg"; };}'
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+        gsettings set org.gnome.desktop.background picture-uri ~/.hasselhoff.jpg
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        defaults write com.apple.desktop Background '{default = {ImageFilePath = "~/.hasselhoff.jpg"; };}'
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+        # ...
+elif [[ "$OSTYPE" == "win32" ]]; then
+        # ...
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
+        # ...
+else
+        # Unknown.
+fi
